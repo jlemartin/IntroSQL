848eaf1f52d531753fb669794f1f54b5c69a8255
John Martin <jlemartin7@gmail.com>
2020-11-23 16:16:09 -0600

    setting up Dapper and Department

---
+++ IntroSQL/DapperDepartmentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+
+namespace IntroSQL
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        // Here is the Constructor
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;").ToList();
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+new { departmentName = newDepartmentName });
+        }
+
+    }
+}
---
+++ IntroSQL/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace IntroSQL
+{
+    public class Department
+    {
+        public Department()
+        {
+        }
+
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
